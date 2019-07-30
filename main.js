6f2f4b9787a042148de2255ad915deddb4665459
jaketrent <trent.jake@gmail.com>
2019-07-30 06:45:10 -0600

    use binding instead of string concat for email where clause

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-const nanoid = require("nanoid")
-const util = require("util");
-const knex = require("knex")({
-  client: "pg",
-  connection: "postgres://postgres:postgres@localhost/test?sslmode=disable"
-});
+const nanoid = require('nanoid')
+const util = require('util')
+const knex = require('knex')({
+  client: 'pg',
+  connection: 'postgres://postgres:postgres@localhost/test?sslmode=disable'
+})
 
 const createUser = async ({ email, name }) => {
-  const insert = knex("users")
+  const insert = knex('users')
     .insert({ email, name })
-    .toString();
+    .toString()
 
-  const update = knex("users")
+  const update = knex('users')
     .update({ name })
-    .whereRaw(`users.email = '${email}'`);
+    .whereRaw('users.email = ?', [email])
   const query = util.format(
-    "%s ON CONFLICT (email) DO UPDATE SET %s",
+    '%s ON CONFLICT (email) DO UPDATE SET %s',
     insert.toString(),
-    update.toString().replace(/^update\s.*\sset\s/i, "")
-  );
+    update.toString().replace(/^update\s.*\sset\s/i, '')
+  )
 
-  await knex.raw(query);
-};
-
-async function main() {
-  await createUser({ email: "test@example.com", name: "Jake " + nanoid() });
-
-  knex.destroy();
+  await knex.raw(query)
 }
 
-main();
+async function main() {
+  await createUser({ email: 'test@example.com', name: 'Jake ' + nanoid() })
+
+  knex.destroy()
+}
+
+main()
